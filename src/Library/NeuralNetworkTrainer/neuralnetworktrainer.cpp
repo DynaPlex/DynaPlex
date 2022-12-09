0e652ccc6d3dc8de00b15794f25361910010a062
Willem van Jaarsveld <wvanjaarsveld@hotmail.com>
2022-12-09 17:19:31 +0100

    works on snellius with pytorch

--- src/Library/NeuralNetworkTrainer/neuralnetworktrainer.cpp
+++ src/Library/NeuralNetworkTrainer/neuralnetworktrainer.cpp
@@ -1,15 +1,15 @@
 #include "dynaplex/neuralnetworktrainer.h"
 #include <iostream>
 #if Torch_available
-#include <torch/torch.h>
+//#include <torch/torch.h>
 #endif
 
 namespace DynaPlex {
 	void NeuralNetworkTrainer::writeidentifier()
 	{
 #if Torch_available
-		torch::Tensor eye = torch::eye(2);
-		std::cout << eye << std::endl;
+	//	torch::Tensor eye = torch::eye(2);
+	//	std::cout << eye << std::endl;
 #endif
 		std::cout << mdp->Identifier() << std::endl;
 	}
